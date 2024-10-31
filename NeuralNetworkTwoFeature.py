5ff1ad3888d8cbc85da05e9f744a99858dc86b94
Hjjj <Wenonaliar@yahoo.com>
2024-10-31 13:51:57 -0600

    Set up some pseudo golf test data

--- NeuralNetworkTwoFeature.py
+++ NeuralNetworkTwoFeature.py
@@ -21,14 +21,21 @@ model.summary()
 
 # Example data (replace with your actual dataset)
 import numpy as np
-x_train = np.array([[0.1, 0.2], [0.4, 0.5], [0.7, 0.8], [0.9, 1.0]])
-y_train = np.array([0, 1, 0, 1])
+
+#x_train = np.array([[0.1, 0.2], [0.4, 0.5], [0.7, 0.8], [0.9, 1.0]])
+#y_train = np.array([0, 1, 0, 1])
+
+#Look at it this way:
+#When the temp is 70 degrees and the wind is 50 mph, Golfing = NO.
+# IF xtrain[70, 50] THEN ytrain = [0]
+x_train = np.array([[70, 50], [70, 0], [70, 10], [70, 45],[70, 8],[80, 42] ])
+y_train = np.array([0, 1, 1, 0, 1, 0])
 
 # Train the model
 model.fit(x_train, y_train, epochs=10, batch_size=1)
 
-# 2 pieced of test data to run through the model
-x_new = np.array([[0.2, 0.3], [0.6, 0.7]])
+# 4 pieces of test data to run through the model
+x_new = np.array([[70, 0], [70, 47], [70,20], [70,13]])
 
 # Make predictions
 predictions = model.predict(x_new)
